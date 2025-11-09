6a4bcb3bcb7fd91f16df6237cce0654d36d3c0a9
May Mossor <benjamin.mossor@student.swic.edu>
2025-11-09 11:12:47 -0600

    Add price change for product 2

--- src/main.js
+++ src/main.js
@@ -30,4 +30,7 @@ document.querySelectorAll(".card").forEach((card) => {
   card.setAttribute("data-viewed", "true");
 });
 
-// Select the product with data-product-id="2" and change its price to "$24.99".
+// 6. Select the product with data-product-id="2" and change its price to "$24.99".
+// I was super close to getting this one on my own, but Warp helped me realize I should use querySelector twice.
+const product2 = document.querySelector("[data-product-id='2']");
+product2.querySelector(".price").textContent = "$24.99";
