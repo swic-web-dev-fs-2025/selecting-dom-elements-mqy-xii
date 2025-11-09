326a130db6a5dbf03bf9b800a8bca9ea37ff1555
May Mossor <benjamin.mossor@student.swic.edu>
2025-11-09 10:41:54 -0600

    Add selector query for all nav links

--- src/main.js
+++ src/main.js
@@ -10,7 +10,10 @@
 const header = document.getElementById("main-header");
 console.info(header);
 
-// Select all links inside .navbar using querySelectorAll and log how many there are.
+// 2. Select all links inside .navbar using querySelectorAll and log how many there are.
+const navLinks = document.querySelectorAll("nav > a");
+console.info(`# of nav links: ${navLinks.length}`);
+
 // Select internal links (those starting with /) and append " [internal]" to their text.
 // Select all required inputs and add a yellow outline (style.outline = "2px solid gold").
 // Select all .card elements, loop through them, and add a data-viewed="true" attribute to each.
